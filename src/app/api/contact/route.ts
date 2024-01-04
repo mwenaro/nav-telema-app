d964f8a71005c226e4675ee7a206c58864c556c2
Mwero Abdalla <mwerothewebmaker@gmail.com>
2024-01-04 15:08:05 +0300

    chore : add mail sender

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -29,13 +29,13 @@ export async function POST(request: Request) {
     const htmlBody = `<div style="font-family: Arial, sans-serif; max-width: 600px; margin: 20px auto; padding: 20px; border: 1px solid #ccc; border-radius: 5px;">
 <h2>Thank You for Contacting Us</h2>
 
-<p style="text-transform:capitalize;">Dear ${name},</p>
+<h3 style="text-transform:capitalize;">Dear ${name},</h3>
 
 <p>Thank you for reaching out to us! We have received your message and appreciate the time you took to contact us. Our team is currently reviewing your inquiry, and we will get back to you as soon as possible.</p>
 
 <p>If your matter requires immediate attention, please feel free to call us at +254 7xx-xxx-xxx. Otherwise, please be assured that we are working diligently to provide you with a thoughtful and comprehensive response.</p>
 
-<p>Once again, thank you for choosing <em>EasyTruck</em>. We value your interest and look forward to assisting you.</p>
+<p>Once again, thank you for choosing <strong><em>EasyTruck</em></strong>. We value your interest and look forward to assisting you.</p>
 
 <p>Best regards,<br>
 <br>
