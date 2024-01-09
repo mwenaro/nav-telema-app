2190db92bce47a85f7f3c49c8ca1c9595cd7a556
Mwero Abdalla <mwerothewebmaker@gmail.com>
2024-01-09 09:12:17 +0300

    fix : update the num of fields in checkpoints

--- src/libs/mongoose/models/checkpoint.ts
+++ src/libs/mongoose/models/checkpoint.ts
@@ -5,27 +5,26 @@ import { EAST_AFRICAN_TOWNS } from "@/data/countries";
 
 export type Checkpoint = {
   _id?: string;
-  type: string;
+  type?: string;
   town: string | any; // Use the ID of the Town as a reference
   name: string;
-  category: string;
+  category?: string;
   geofenceCoordinates?: string;
   latitude: number;
   longitude: number;
   description: string;
-  tolerance: number;
-  radius: number;
-  geofenceType: string;
+  tolerance?: number;
+  radius?: number;
   contactNo: string;
   address: string;
 };
 
 // Yup Validation Schema
 const checkpointSchemaValidation = yup.object().shape({
-  type: yup.string().required("Type is required"),
+  // type: yup.string(),
   town: yup.string().required("Town is required"),
   name: yup.string().required("Name is required"),
-  category: yup.string().required("Category is required"),
+  // category: yup.string().required("Category is required"),
   // geofenceCoordinates: yup
   //   .string()
   //   .required("Geofence Coordinates are required"),
@@ -38,35 +37,24 @@ const checkpointSchemaValidation = yup.object().shape({
     .number()
     .typeError("Tolerance must be a number")
     .required("Tolerance is required"),
-  tolerance: yup
-    .number()
-    .typeError("Tolerance must be a number")
-    .required("Tolerance is required")
-    .positive("Tolerance must be a positive number"),
-  radius: yup
-    .number()
-    .typeError("Radius must be a number")
-    .required("Radius is required")
-    .positive("Radius must be a positive number"),
-  geofenceType: yup.string().required("Geofence Type is required"),
+
   contactNo: yup.string().required("Contact No is required"),
   address: yup.string().required("Address is required"),
 });
 
 const checkpointSchema = new mongoose.Schema<Checkpoint>(
   {
-    type: { type: String, required: true },
+    type: { type: String, default: "Checkpoint" },
     // town: { type: Schema.Types.ObjectId, ref: "Town", required: true },
     town: { type: String, required: true },
     name: { type: String, required: true },
-    category: { type: String, required: true },
+    category: { type: String, default: "" },
     geofenceCoordinates: { type: String, default: "" },
     latitude: { type: Number, required: true },
     longitude: { type: Number, required: true },
     description: { type: String, required: true },
-    tolerance: { type: Number, required: true },
-    radius: { type: Number, required: true },
-    geofenceType: { type: String, required: true },
+    tolerance: { type: Number, required: true, default: 0 },
+    radius: { type: Number, required: true, default: 30 },
     contactNo: { type: String, required: true },
     address: { type: String, required: true },
   },
@@ -84,7 +72,6 @@ const initialCheckpointValues: Checkpoint = {
   description: "",
   tolerance: 0, // Set to the default tolerance or leave it at 0
   radius: 0, // Set to the default radius or leave it at 0
-  geofenceType: "",
   contactNo: "",
   address: "",
 };
@@ -92,8 +79,8 @@ const initialCheckpointValues: Checkpoint = {
 const checkpointFormFields = [
   // Populate with Towns dynamically
   { name: "name", label: "Name", type: "text", labeled: true },
-  { name: "type", label: "Type", type: "text", labeled: true },
-  { name: "category", label: "Category", type: "text", labeled: true },
+  // { name: "type", label: "Type", type: "text", labeled: true },
+  // { name: "category", label: "Category", type: "text", labeled: true },
   {
     name: "town",
     label: "Town",
@@ -106,14 +93,6 @@ const checkpointFormFields = [
 
   { name: "longitude", type: "number", label: "Longitude", labeled: true },
   { name: "description", label: "Description", type: "text", labeled: true },
-  {
-    name: "tolerance",
-    label: "Tolerance (meter)",
-    type: "text",
-    labeled: true,
-  },
-  { name: "radius", label: "Radius (meter)", type: "text", labeled: true },
-  { name: "geofenceType", label: "Geofence Type", type: "text", labeled: true },
   { name: "contactNo", label: "Contact No", type: "text", labeled: true },
   { name: "address", label: "Address", type: "text", labeled: true },
 ];
